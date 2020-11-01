7e235fcc1988bac2709b5ea6673121d644c73b04
saadsafda <muhammadsaadsafdar2005@gmail.com>
2020-11-01 08:58:06 +0500

    First node blog for github

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+// const fs = require("fs");
+// const PORT = process.env.PORT || 5000;
+// //set ejs
+// app.set("views", "ejs")
+// //ruotes
+// app.get("/", (req, res) => {
+//     res.render("index.ejs");
+// })
+// //cerat server
+// app.listen(PORT, () => {
+//     console.log(`sever running on port number: ${PORT}`)
+// });
